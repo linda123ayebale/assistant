648ed5e3a6e41902a9a10756cb9877100a64447a
bradiepoa <bradiepoa7@gmail.com>
2023-06-07 01:24:25 +0300

    restricting pages using login required decorator

--- ar/views.py
+++ ar/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='accounts:login')
 def homeviews(request):
     return render(request, "ar/index.html")
