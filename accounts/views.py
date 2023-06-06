9b1a21c5f23f699b91758d5de2980dc59d2f2ed7
bradiepoa <bradiepoa7@gmail.com>
2023-06-06 23:40:26 +0300

    add login view logic working properly

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 # Create your views here.
 
@@ -6,6 +6,11 @@ def loginViews(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect("chatbot:home")
 
     return render(request, 'accounts/login.html')
 
